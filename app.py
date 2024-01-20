405fb321e1120ab6ff9ad47166015aff56b5bd96
DevangPagare002 <devpagare002@gmail.com>
2024-01-21 00:27:46 +0530

    Write function to get response from gemini-pro model

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ import streamlit
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv() ## Load my environment variables.
 
 genai.configure(api_key = os.getenv("GOOGLE_API_KEY")) ## Configuring the API key
+
+def get_gemini_response(input):
+    model = genai.GenerativeModel("gemini-pro") ## Calling Gemini model
+    response = model.generate_content(input)
+    response = json.loads(response.text)
+    return response
