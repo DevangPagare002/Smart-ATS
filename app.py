adcf3fe9faa977203c9a0986dfd7e05111ffa9f3
DevangPagare002 <devpagare002@gmail.com>
2024-01-21 12:59:02 +0530

    Improve prompt and spelling mistakes.

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ load_dotenv() ## Load my environment variables.
 
 genai.configure(api_key = os.getenv("GOOGLE_API_KEY")) ## Configuring the API key
 
+## Below function gets the response from gemini-pro
 def get_gemini_response(input):
     model = genai.GenerativeModel("gemini-pro") ## Calling Gemini model
     response = model.generate_content(input)
     return response.text
 
+## Below function reads text from pdf
 def read_text_from_pdf(uploaded_files):
     reader = pdf.PdfReader(uploaded_files) ## Read the pdf
     text = ""
@@ -34,9 +36,10 @@ uploaded_files = st.file_uploader("Upload your resume", type="pdf", help="Please
 
 text = read_text_from_pdf(uploaded_files)
 
+## Input prompt
 input_prompt = f"""
 Hii, Act like you are a very skillful or experienced ATS(Application Tracking System).
-You have deep understanding of tech field, spftware engineering, data science, data analysis and big data
+You have deep understanding of tech field, software engineering, data science, data analysis and big data
 engineer. Your task is to evaluate the resume based on given job description.
 You must consider that the job market is very competitive and you should provide bjest assistance for improving the resume according to job description.
 Assign the percentage matching  based on job description and the missing keyworkds with high accuracy. \n
@@ -47,7 +50,8 @@ I want the response in one single string having a json structure like -
 {{"Job description Match" : "%", 
  "Missing Keywords from resume" : "[]", 
  "job description Summary" : "", 
- "Resume summary" : ""}}
+ "Resume summary" : "",
+ "Is candidate fit for job" : "yes/no"}}
 """
 
 submit = st.button("Submit")
