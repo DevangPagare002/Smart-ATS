f81c9e3361c7e26f65236b157220d32f5a9fab30
DevangPagare002 <devpagare002@gmail.com>
2024-01-21 12:32:37 +0530

    Improve prompt and add streamlit interface.

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import streamlit
+import streamlit as st
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
-import json
 import PyPDF2 as pdf
 
 load_dotenv() ## Load my environment variables.
@@ -12,8 +11,7 @@ genai.configure(api_key = os.getenv("GOOGLE_API_KEY")) ## Configuring the API ke
 def get_gemini_response(input):
     model = genai.GenerativeModel("gemini-pro") ## Calling Gemini model
     response = model.generate_content(input)
-    response = json.loads(response.text)
-    return response
+    return response.text
 
 def read_text_from_pdf(uploaded_files):
     reader = pdf.PdfReader(uploaded_files) ## Read the pdf
@@ -24,15 +22,38 @@ def read_text_from_pdf(uploaded_files):
 
     return text
 
-input_prompt = """
+
+## Streamlit app
+st.title("Smart ATS")
+
+st.text("Improve your resume by ATS")
+
+jd = st.text_area("Paste the job description")
+
+uploaded_files = st.file_uploader("Upload your resume", type="pdf", help="Please upload the pdf")
+
+text = read_text_from_pdf(uploaded_files)
+
+input_prompt = f"""
 Hii, Act like you are a very skillful or experienced ATS(Application Tracking System).
 You have deep understanding of tech field, spftware engineering, data science, data analysis and big data
 engineer. Your task is to evaluate the resume based on given job description.
 You must consider that the job market is very competitive and you should provide bjest assistance for improving the resume according to job description.
-Assign the percentage matching  based on job description and the missing keyworkds with high accuracy.
-resume : {text}
-description : {jd}
+Assign the percentage matching  based on job description and the missing keyworkds with high accuracy. \n
+__resume__ : {text}\n
+__description__ : {jd}
 
-I want the response in one single string having a structure like -  
-{{"JD Match" : "%", "MissingKeywords" : [], "Profile Summary" : ""}}
+I want the response in one single string having a json structure like -  
+{{"Job description Match" : "%", 
+ "Missing Keywords from resume" : "[]", 
+ "job description Summary" : "", 
+ "Resume summary" : ""}}
 """
+
+submit = st.button("Submit")
+
+if submit:
+    if uploaded_files is not None:
+        text = read_text_from_pdf(uploaded_files)
+        response = get_gemini_response(input_prompt)
+        st.subheader(response)
